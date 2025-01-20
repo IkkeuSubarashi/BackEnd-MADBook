e40c06774e440f73eda0792e327297b0e734a0bd
Wapi <wafiq.smartstudet@gmail.com>
2025-01-20 09:44:19 +0800

    Model Update 3

--- app/Models/quotations.php
+++ app/Models/quotations.php
@@ -8,9 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class quotations extends Model
 {
     use HasFactory;
+
     public $timestamps = false;
+    protected $fillable = [
+        'logo',
+        'subject',
+        'valid_date',
+        'status',
+        'total',
+        'created_at',
+        'c_name',
+        'c_address',
+        'c_no',
+        'borrower_id',
+    ];
+
     public function borrowers(){
-        return $this->belongsTo(borrowers::class,'borrower_id');
+        return $this->belongsTo(borrower::class,'borrower_id');
     }
     public function q_items(){
         return $this->belongsToMany(q_items::class,'quote_items','quote_id','item_id');
